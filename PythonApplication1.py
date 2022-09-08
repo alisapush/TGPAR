d01f3e7fb693ad83c38262d125b96665ef3ffeee
syrym94 <syrym94@gmail.com>
2022-09-08 20:32:32 +0600

    request done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -7,6 +7,7 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 from telethon import events
 from telethon import connection
+from telethon import functions, types
 
 # для корректного переноса времени сообщений в json
 from datetime import date, datetime
@@ -38,6 +39,16 @@ client = TelegramClient(username, api_id, api_hash)
 
 #channel = await client(functions.messages.CheckChatInviteRequest(hash = "W-C6i5Co-7I5ZTk6")).chat
 
+
+class Post:
+    def __init__(self, channelId, channelTitle, text, author, datetime):
+        self.channelId = channelId
+        self.channelTitle = channelTitle
+        self.text = text
+        self.author = author
+        self.datetime = datetime
+
+
 @client.on(events.NewMessage(chats=urls))
 async def handler_new_message(event):
     try:
@@ -45,8 +56,61 @@ async def handler_new_message(event):
         # if "js" in event.raw_text or "Js" in event.raw_text or "JS" in event.raw_text:
         res = None
         res = await client.forward_messages(my_channel, event.message)
+        print("====== POST START ====== ")
+
+
+        channelId = str(event.peer_id)
+
+        ch = '='
+
+        channelConstId = '-100'
+        channelId = channelConstId + \
+            channelId.split(ch, 1)[1].rstrip(channelId[-1])
+
+        channelFull = await client(functions.channels.GetFullChannelRequest(channel=int(channelId)))
+        channelTitle = str(channelFull)
+        start = "title='"
+        end = "'"
+        channelTitle = channelTitle.split(start, 1)[1].split(end, 1)[0]
+        text = str(event.raw_text)
+        author = str(event.post_author)
+        datetime = str(event.date)
+
+        # разница во времени с Минском 3 часа
+
+        # datetime = str(event.date + timedelta(hours=3))
+
+
+        p = Post(channelId, channelTitle, text, author, datetime)
+        print("Channel id: ")
+        print(p.channelId)
+        print("Channel title: ")
+        print(p.channelTitle)
+        print("Message text: ")
+        print(p.text)
+        print("Post author: ")
+        print(p.author)
+        print("Date time: ")
+        print(p.datetime)
+        print("====== POST END ====== ")
+        # Data to be written
+        dictionary = {
+
+            "id": channelId,
+
+            "message": channelTitle,
+
+            "channel": author,
+
+            "text": text,
+
+            "date": datetime
+
+        }
+        # Serializing json
+        json_object = json.dumps(dictionary, indent=5, ensure_ascii=False)
         if res is not None:
-            await use_requests(node_url)
+            await use_requests(node_url, {"message": channelTitle, "channel": author, "time": datetime})
 
     except Exception as e:
         print(e)
--- req.py
+++ req.py
@@ -7,9 +7,9 @@ import requests
 
 api_url = 'https://dummyjson.com/products'
 
-def use_requests(api_url):
+def use_requests(api_url, post):
 
-    response = requests.get(api_url)
+    response = requests.post(api_url, post)
     json_response = json.loads(response.text)
     print(json_response)
     return
